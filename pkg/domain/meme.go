20637d6f72d70e668bab08a5df1fb7b90fe8d367
zhews <118117386+zhews@users.noreply.github.com>
2022-12-11 16:17:55 +0100

    feat: add repository interface for meme

--- pkg/domain/meme.go
+++ pkg/domain/meme.go
@@ -3,9 +3,18 @@ package domain
 import "github.com/google/uuid"
 
 type Meme struct {
+	Id        uuid.UUID
 	Title     string
 	Image     string
 	CreatedBy uuid.UUID
 	CreatedAt int64
 	UpdatedAt int64
+}
+
+type MemeRepository interface {
+	GetAll() ([]Meme, error)
+	GetById(id uuid.UUID) (User, error)
+	Insert(meme Meme) error
+	Update(meme Meme) error
+	Delete(id uuid.UUID) error
 }
