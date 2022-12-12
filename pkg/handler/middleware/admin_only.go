48421b36fd88b556f6a0aca09aa37b85d4461ae3
zhews <118117386+zhews@users.noreply.github.com>
2022-12-12 12:19:53 +0100

    feat: improve logging of middleware

--- pkg/handler/middleware/admin_only.go
+++ pkg/handler/middleware/admin_only.go
@@ -3,11 +3,13 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	"log"
 )
 
 func AdminOnly(ctx *fiber.Ctx) error {
 	token, ok := ctx.Locals("user").(*jwt.Token)
 	if !ok {
+		log.Println("Could not parse token from context")
 		return ctx.SendStatus(fiber.StatusInternalServerError)
 	}
 	claims := token.Claims.(jwt.MapClaims)
